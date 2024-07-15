c476ac525471a29a6fd361d75822e49d4c1c25cb
pedro lavor <pedrinho.lavor@aluno.uece.br>
2024-07-15 20:57:28 -0300

    main v0.1.3

---
+++ mecadinho.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+
+int main(void){
+    printf("Ola, bem-vindo ao mercadinho da Maria!");
+}
--- mercado.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-
-int main(void){
-    printf("Ola, bem-vindo ao mercado da dona Maria!");
-}
